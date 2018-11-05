426a8fa4fee988abef6d65a00bdb330911a2d430
Ezequiel Ramos <ezequielmr94@gmail.com>
2018-11-05 14:05:07 -0200

    VITORIAAAAAAAAAA

--- python/darknet.py
+++ python/darknet.py
@@ -45,7 +45,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/ezequiel/Documents/Projects/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -148,9 +148,9 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
+    net = load_net(b"/home/ezequiel/Documents/Projects/darknet/cfg/yolov3-320.cfg", b"/home/ezequiel/Documents/Projects/darknet/weights/yolov3-320.weights", 0)
+    meta = load_meta(b"/home/ezequiel/Documents/Projects/darknet/cfg/coco.data")
+    r = detect(net, meta, b"/home/ezequiel/Documents/Projects/darknet/data/dog.jpg")
+    print(r)
     
 
